a04b9f1e78875d4548bfa98ca2515aadec012a0c
lizhenghui <li12233@qq.com>
2018-08-05 19:22:23 +0800

    2018/8/5更新

---
+++ my_pc/src/components/header_li.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../css/header_li.css'
+import {BrowserRouter as Router,Link,Route,Redirect,Switch}from 'react-router-dom'
+import logo from '../img/logo.png'
+
+
+class Header_li extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <Router>
+                <div id={"header_wrap_lzh"}>
+                    <div className="fl header_fl_lzh">
+                        <Link to="/home" className="fl">
+                            <img src={logo} alt=""/>
+                        </Link>
+                        <ul className="fl header_nav_lzh">
+                            <li><Link to="/home">首页</Link></li>
+                            <li><Link to="/home">搭配</Link></li>
+                            <li className="zg_li_lzh"><Link to="/home">商家</Link><span className="zg_span_lzh">大厂直供</span></li>
+                            <li><Link to="/home">灵感</Link></li>
+                        </ul>
+                        <div className="fl header_ss_lzh">
+                            <Link to="/home" className="header_icon_lzh"></Link>
+                        </div>
+                    </div>
+                    <div className="fr header_fr_lzh">
+                        <div className="fl header_fr_tp_lzh">
+                            <Link to="/home">
+                                <span className="fl header_fr_tp_span1_lzh"></span>
+                                <span>创建搭配</span>
+                            </Link>
+                        </div>
+                        <div className="fl header_fr_login_lzh">
+                            <i></i>
+                            <em></em>
+                        </div>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
+}
+
+
+export default Header_li
--- my_pc/src/index.js
+++ my_pc/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header_li from './components/header_li'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header_li />, document.getElementById('root'));
 registerServiceWorker();
