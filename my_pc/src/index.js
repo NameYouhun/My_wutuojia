248be5bc16b265027e0d098aa47a6c5bfcd71602
wang <1053405833@qq.com>
2018-08-06 15:42:27 +0800

    添加推荐商品

--- my_pc/src/index.js
+++ my_pc/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header_li from './components/header_li'
+import Main_wang from './components/main_wang'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Header_li />, document.getElementById('root'));
+ReactDOM.render(<Main_wang />, document.getElementById('root'));
 registerServiceWorker();
