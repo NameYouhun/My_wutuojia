2493e06e8736dd825dff698df9d5e3cecf8e46c9
lizhenghui <li12233@qq.com>
2018-08-06 17:54:59 +0800

    8/6--下午

--- my_pc/src/index.js
+++ my_pc/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header_li from './components/header_li'
-import Main_wang from './components/main_wang'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header_li />, document.getElementById('root'));
-ReactDOM.render(<Main_wang />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
