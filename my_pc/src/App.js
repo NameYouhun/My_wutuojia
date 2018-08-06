487aca357493d0e07022f41487e03d167c7451ee
smhsmh <1958047639@qq.com>
2018-08-06 22:53:03 +0800

    史明辉

--- my_pc/src/App.js
+++ my_pc/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
                   <Route path="/idea" component={Idea}></Route>
                   <Redirect to="/home"/>
               </Switch>
+              
           </div>
         </Router>
     );
--- my_pc/src/index.js
+++ my_pc/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header_li from './components/header_li'
+import Header_li from './components/header_li';
+import Main_smh from './components/main_smh';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Header_li />, document.getElementById('root'));
+ReactDOM.render(<Main_smh />, document.getElementById('root'));
 registerServiceWorker();
