a2eda6f367a69321eb75b5fe773d111d13ba9228
smhsmh <1958047639@qq.com>
2018-08-07 15:24:15 +0800

    ..

---
+++ my_pc/src/components/fangan_smh.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import $ from 'jquery'
+import {BrowserRouter as Router,Route,Link,Redirect,Switch} from 'react-router-dom'
+import '../css/fangan_smh.css'
+
+
+
+class Fangan_smh extends React.Component{
+	
+	constructor(props){
+		super(props)
+	}
+	
+	render(){
+		return(
+			<div>
+				
+			</div>
+		)
+	}
+}
+export default Fangan_smh
