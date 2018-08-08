4896237ce936394e4b847b2324984b014711b46b
PengJunLiang <JLPeng@163.com>
2018-08-08 15:03:18 +0800

    写好了designer里面的设计师列表

--- my_pc/src/components/Sjs_peng.js
+++ my_pc/src/components/Sjs_peng.js
@@ -1,5 +1,8 @@
 import React from "react";
 import '../css/sjs_peng.css';
+import { Pagination } from 'antd';
+
+
 
 class Sjs_peng extends React.Component{
 	constructor(props){
@@ -22,18 +25,194 @@ class Sjs_peng extends React.Component{
 							</div>
 						</div>
 						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
 						
-					
 					</li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
-					<li className='list_pjl'></li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					<li className='list_pjl'>
+						<div className='head_pjl'>
+							<div className="guanzhu_pjl">关注</div>
+							<div className="toux_pjl">
+							</div>
+						</div>
+						<div className="name_pjl">鱼</div>
+						<div className="pro1_pjl"></div>
+						<div className='clear_pjl'>
+							<span className='xtit_pjl'>122搭配</span>
+							<span className='xtit_pjl'>31粉丝</span>
+						</div>
+						<div className='spic_pjl'>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155904_676606.png")} className="pic2_pjl"/>
+							</div>
+							<div className='pouter_pjl'>
+								<img src={require("../img/155416_236050.png")} className="pic2_pjl"/>
+							</div>
+						</div>
+						
+					</li>
+					
+					
+					
 				</ul>
-				
+				<Pagination defaultCurrent={1} total={50} className='page_pjl'/>
 				
 				
 			</div>
